e9c8e364b30ac17be2841d0073368051215f665e
fitchzheng <support@wengaoelec.com>
2026-07-03 11:12:08 +0800

    更改自动识别逻辑和电流显示滤波

--- code/user/L1/data_com.c
+++ code/user/L1/data_com.c
@@ -328,12 +328,39 @@ static float filter_show_value_adaptive(float old_value,
     return (0.85f * old_value) + (0.15f * new_value);
 }
 
+static float filter_show_current_value(float old_value, float new_value)
+{
+    float delta = fabsf(new_value - old_value);
+
+    if ((old_value < 0.05f) || (new_value < 0.05f))
+    {
+        return new_value;
+    }
+
+    if (delta < 0.20f)
+    {
+        return old_value;
+    }
+
+    if (delta >= 10.0f)
+    {
+        return (0.40f * old_value) + (0.60f * new_value);
+    }
+
+    if (delta >= 1.0f)
+    {
+        return (0.85f * old_value) + (0.15f * new_value);
+    }
+
+    return (0.95f * old_value) + (0.05f * new_value);
+}
+
 void data_com_run(void)
 {
     filter_show_data_value.fvs48_lmt = filter_show_value_adaptive(filter_show_data_value.fvs48_lmt, get_show_fvs48_show(), 1.0f, 0.2f);
-    filter_show_data_value.ihv_lmt   = filter_show_value_adaptive(filter_show_data_value.ihv_lmt, fabsf(get_show_ihv_show()), 1.0f, 0.1f);
+    filter_show_data_value.ihv_lmt   = filter_show_current_value(filter_show_data_value.ihv_lmt, fabsf(get_show_ihv_show()));
     filter_show_data_value.rvs12_lmt = filter_show_value_adaptive(filter_show_data_value.rvs12_lmt, get_show_rvs12_show(), 1.0f, 0.2f);
-    filter_show_data_value.ilv_lmt   = filter_show_value_adaptive(filter_show_data_value.ilv_lmt, fabsf(get_show_ilv_show()), 1.0f, 0.1f);
+    filter_show_data_value.ilv_lmt   = filter_show_current_value(filter_show_data_value.ilv_lmt, fabsf(get_show_ilv_show()));
 
     //WG_COM_V2_GET_DATA(fvs48_lmt, wg_com_v2_param.SetInpVolt);
     //WG_COM_V2_GET_DATA(ihv_lmt, wg_com_v2_param.SetInpCurr);
